fe6d02eaea753d876c399082f1ebc656ef33f49e
Cole Turner <turner.cole@gmail.com>
2016-06-24 13:38:27 -0400

    Fixed regex

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# [Webpack](http://webpack.github.io/) loader to append text to files
+# [Webpack](http://webpack.github.io/) loader expose class names
 
 This loader will detect JavaScript class names and expose them to the global context.
+
+To use this loader, specify the `query` parameter as a string for the object to attach the class.
+Example: `global`, `window`
 
 # Why?
 
--- index.js
+++ index.js
@@ -2,25 +2,26 @@ module.exports = function(source, map) {
   this.cacheable();
 
   var callback = this.async();
-  var query = this.query;
-  if(typeof query == 'string')
-  {
-    query = { scope: query };
-  }
+  var query = this.query && this.query != "?" ? this.query.substring(1) : "global";
 
   source += "\n";
 
-  var regex = new RegExp("class ([a-zA-Z0-9]+)((\s*)extends [a-zA-Z0-9]+)*(\s*){", "gm");
+  var regex = /class\s*([a-zA-Z0-9\.\_\-]+)((\s*)extends(\s*)[a-zA-Z0-9\.\_\-]+)*(\s*){/gmi;
   var matches = source.match(regex);
+
   if(matches)
   {
-      matches.forEach(function(match)
+    var match;
+    while (match = regex.exec(source))
+    {
+      if(match.length > 1)
       {
         var className = match[1];
 
-        var append = "if(typeof "+className+" != 'undefined') { "+query.scope+"."+className+" = "+className+"; }\n";
+        var append = "if(typeof "+className+" != 'undefined') { "+query+"."+className+" = "+className+"; }\n";
         source += append;
-      });
+      }
+    }
   }
 
   callback(null, source, map)
